dc3c36bb66089c3a8289fc7fbaad436cacf7c1f1
CaioARosa <caioaugustorosa@gmail.com>
2021-03-31 16:41:33 -0300

    Storage via cookies

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+
     return(
         <div className={styles.profileContainer}>
             <img src='https://instagram.fsdu12-1.fna.fbcdn.net/v/t51.2885-19/s150x150/109484061_202448714528749_709228686027526500_n.jpg?tp=1&_nc_ht=instagram.fsdu12-1.fna.fbcdn.net&_nc_ohc=-Q9Tp_c7aboAX8iegMO&ccb=7-4&oh=09fc72d52ee4936a7e4ce1e0a3629b3f&oe=60834B26&_nc_sid=7bff83' alt='Caio Augusto'/>
@@ -8,7 +13,7 @@ export function Profile() {
                 <strong>Caio Augusto</strong>
                 <p>
                     <img src="icons/level.svg" alt=""/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
